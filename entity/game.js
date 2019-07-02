49ee4c333e835d2a1b7022fe2ed14e087ddd8272
Yangjian <yangjian@1312.com>
2019-07-02 17:55:43 +0800

    1、绝杀判断（单子将）2、返回棋子能到达的位置时也返回它自己的位置

--- entity/game.js
+++ entity/game.js
@@ -15,18 +15,17 @@ class Game {
   canGo(x, y) {
     for (let i = 0; i < this.wayCanGo.length; i++) {
       const pos = this.wayCanGo[i];
-      if (pos[0] == x && pos[1] == y) {
+      if (pos.x == x && pos.y == y) {
         return true;
       }
     }
     return false;
   }
 
-  // 移动某一个棋子后是否正在被将军
-  moveToDead(x, y) {
+  // selectChess 移动到 x, y 后是否正在被将军
+  moveToDead(selectChess, x, y) {
     const cChessArr = _.cloneDeep(chessArr);
     const cChessObjArr = _.cloneDeep(chessObjArr);
-    const selectChess = game.selectChess;
     const { x: lx, y: ly } = selectChess;
     this.lastPos = [lx, ly];
     cChessArr[lx][ly] = EMPTY_POS;
@@ -47,7 +46,7 @@ class Game {
       for (let j = 0; j < chessObjArr[i].length; j++) {
         let chess = chessObjArr[i][j];
         if (chess != EMPTY_POS && chess.chessType == CHESS_TYPE.JIANG && chess.camp == camp) {
-          return [i, j];
+          return chess;
         }
       }
     }
@@ -68,7 +67,7 @@ class Game {
     // 判断老将在不在对方任一棋子能到达的地方
     const jiangPos = this.getJiangPos(chessObjArr, camp);
     for (const pos of positions) {
-      if ((pos[0] == jiangPos[0] && pos[1] == jiangPos[1]) || this.checkJiangMeet(chessObjArr)) {
+      if ((pos.x == jiangPos.x && pos.y == jiangPos.y) || this.checkJiangMeet(chessObjArr)) {
         return true;
       }
     }
@@ -78,13 +77,13 @@ class Game {
   // 判断老将是否碰面
   checkJiangMeet(chessObjArr) {
     const redJiangPos = this.getJiangPos(chessObjArr, CAMP.RED);
-    const BlackJiangPos = this.getJiangPos(chessObjArr, CAMP.BLACK);
-    if (redJiangPos[0] != BlackJiangPos[0]) {
+    const blackJiangPos = this.getJiangPos(chessObjArr, CAMP.BLACK);
+    if (redJiangPos.x != blackJiangPos.x) {
       return false;
     }
-    for (let i = 0; i < chessObjArr[redJiangPos[0]].length; i++) {
-      for (let j = BlackJiangPos[1] + 1; j < redJiangPos[1]; j++) {
-        const chess = chessObjArr[redJiangPos[0]][j];
+    for (let i = 0; i < chessObjArr[redJiangPos.x].length; i++) {
+      for (let j = blackJiangPos.y + 1; j < redJiangPos.y; j++) {
+        const chess = chessObjArr[redJiangPos.x][j];
         if (chess != EMPTY_POS) {
           return false;
         }
@@ -95,6 +94,157 @@ class Game {
 
   // 是否绝杀
   checkJueSha(camp, chessObjArr) {
-    // TODO
+    const myJiang = this.getJiangPos(chessObjArr, camp);
+    const jiangCangoPos = myJiang.findWayCanGo(chessObjArr);
+    const threatenPos = []; // 敌方棋子威胁的区域
+    const myChessCanGoPos = []; // 我方棋子能到的位置
+    for (let i = 0; i < chessObjArr.length; i++) {
+      for (let j = 0; j < chessObjArr[i].length; j++) {
+        let chess = chessObjArr[i][j];
+        if (chess != EMPTY_POS && chess.camp != camp) {
+          if (chess.chessType == CHESS_TYPE.PAO) {
+            threatenPos.push(...chess.findThreatenPos(chessObjArr));
+          } else {
+            threatenPos.push(...chess.findWayCanGo(chessObjArr));
+          }
+        }
+        if (chess != EMPTY_POS && chess.camp == camp) {
+          myChessCanGoPos.push(...chess.findWayCanGo(chessObjArr));
+        }
+      }
+    }
+    let safePos = 0;
+    for (const jcp of jiangCangoPos) {
+      if (!this.moveToDead(myJiang, jcp.x, jcp.y)) {
+        safePos++;
+        break;
+      }
+    }
+
+    // 有安全位置
+    if (jiangCangoPos.length && safePos) {
+      return false;
+    }
+
+    // 老将动不了 看看能不能把将的子吃掉或者阻挡
+    const threatenChesses = []; // 威胁老将的子
+    for (const tp of threatenPos) {
+      if (tp.x == myJiang.x && tp.y == myJiang.y) {
+        threatenChesses.push(tp.chess);
+      }
+    }
+
+    // 单将
+    if (threatenChesses.length == 1) {
+      const tChess = threatenChesses[0];
+      // 能不能吃了它
+      for (const mccgp of myChessCanGoPos) {
+        if (mccgp.x == tChess.x && mccgp.y == tChess.y) {
+          return false;
+        }
+      }
+
+      // 能不能挡住或者卡马腿
+      if (tChess.chessType == CHESS_TYPE.CHE) {
+        if (tChess.x == myJiang.x) {
+          const miny = Math.min(tChess.y, myJiang.y);
+          const maxy = Math.max(tChess.y, myJiang.y);
+          for (const mccgp of myChessCanGoPos) {
+            if (mccgp.x == tChess.x && mccgp.y > miny && mccgp.y < maxy) {
+              return false;
+            }
+          }
+        } else if (tChess.y == myJiang.y) {
+          const minx = Math.min(tChess.x, myJiang.x);
+          const maxx = Math.max(tChess.x, myJiang.x);
+          for (const mccgp of myChessCanGoPos) {
+            if (mccgp.y == tChess.y && mccgp.x > minx && mccgp.x < maxx) {
+              return false;
+            }
+          }
+        }
+      } else if (tChess.chessType == CHESS_TYPE.MA) {
+        let maLeg;
+        if ((tChess.x - myJiang.x == 1 && tChess.y - myJiang.y == 2) || (tChess.x - myJiang.x == 2 && tChess.y - myJiang. y == 1)) {
+          maLeg = { x: myJiang.x + 1, y: myJiang.y + 1 };
+        } else if ((tChess.x - myJiang.x == -1 && tChess.y - myJiang.y == 2) || (tChess.x - myJiang.x == -2 && tChess.y - myJiang. y == 1)) {
+          maLeg = { x: myJiang.x - 1, y: myJiang.y + 1 };
+        } else if ((tChess.x - myJiang.x == -1 && tChess.y - myJiang.y == -2) || (tChess.x - myJiang.x == -2 && tChess.y - myJiang. y == -1)) {
+          maLeg = { x: myJiang.x - 1, y: myJiang.y - 1 };
+        } else if ((tChess.x - myJiang.x == 1 && tChess.y - myJiang.y == -2) || (tChess.x - myJiang.x == 2 && tChess.y - myJiang. y == -1)) {
+          maLeg = { x: myJiang.x + 1, y: myJiang.y - 1 };
+        }
+
+        for (const mccgp of myChessCanGoPos) {
+          if (mccgp.x == maLeg.x && mccgp.y == maLeg.y) {
+            return false;
+          }
+        }
+      } else if (tChess.chessType == CHESS_TYPE.PAO) {
+        const canBlockPos = [];
+        if (tChess.paojia.chessType == CHESS_TYPE.PAO && tChess.paojia.camp == tChess.camp) {
+          if (tChess.x == tChess.paojia.x) {
+            const miny = Math.min(tChess.y, tChess.paojia.y);
+            const maxy = Math.max(tChess.y, tChess.paojia.y);
+            for (let i = miny + 1; i < maxy; i++) {
+              canBlockPos.push([tChess.x, i]);
+            }
+          } else if (tChess.y == tChess.paojia.y) {
+            const minx = Math.min(tChess.x, tChess.paojia.x);
+            const maxx = Math.max(tChess.x, tChess.paojia.x);
+            for (let i = minx + 1; i < maxx; i++) {
+              canBlockPos.push([tChess.y, i]);
+            }
+          }
+        } else {
+          if (tChess.x == myJiang.x) {
+            if (tChess.y < myJiang.y) {
+              for (let i = tChess.y + 1; i < tChess.paojia.y; i++) {
+                canBlockPos.push({ x: tChess.x, y: i });
+              }
+              for (let i = tChess.paojia.y + 1; i < myJiang.y; i++) {
+                canBlockPos.push({ x: tChess.x, y: i });
+              }
+            } else {
+              for (let i = myJiang.y + 1; i < tChess.paojia.y; i++) {
+                canBlockPos.push({ x: tChess.x, y: i });
+              }
+              for (let i = tChess.paojia.y + 1; i < tChess.y; i++) {
+                canBlockPos.push({ x: tChess.x, y: i });
+              }
+            }
+          } else if (tChess.y == myJiang.y) {
+            if (tChess.x < myJiang.x) {
+              for (let i = tChess.x + 1; i < tChess.paojia.x; i++) {
+                canBlockPos.push({ x: i, y: tChess.y });
+              }
+              for (let i = tChess.paojia.x + 1; i < myJiang.x; i++) {
+                canBlockPos.push({ x: i, y: tChess.y });
+              }
+            } else {
+              for (let i = myJiang.x + 1; i < tChess.paojia.x; i++) {
+                canBlockPos.push({ x: i, y: tChess.y });
+              }
+              for (let i = tChess.paojia.x + 1; i < tChess.x; i++) {
+                canBlockPos.push({ x: i, y: tChess.y });
+              }
+            }
+          }
+        }
+
+        for (const cbp of canBlockPos) {
+          for (const mccgp of myChessCanGoPos) {
+            if (mccgp.x == cbp.x && mccgp.y == cbp.y) {
+              return false;
+            }
+          }
+        }
+      }
+    }
+
+    // 双将
+    if (threatenChesses.length == 2) {
+      // TODO
+    }
   }
 }
